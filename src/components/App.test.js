4e1a6a657ffa0c1ea3f90d5422004f68b9cd8d73
Oleh Milanko <oleh_milanko@epam.com>
2019-03-01 16:13:55 +0200

    [Tests] Endup with tests configuration (mocks for static and styles added)

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../src/components/App';
+import App from './App';
 
 describe('App', () => {
     test('Should match snapshot', () => {
         const wrapper = shallow(<App />);
 
         expect(wrapper.find('h2').text()).toBe('React GMP project');
-        expect(wrapper).toMatchSnapshot;
+        expect(wrapper).toMatchSnapshot();
     })
 });
